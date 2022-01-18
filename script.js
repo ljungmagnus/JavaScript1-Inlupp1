fcb3a686033f0afd5a6bad721eda8ad3e7c42eb2
Magnus Ljung <ljung.magnus@gmail.com>
2022-01-18 16:28:04 +0100

    validering och listan skrivs ut på sidan

--- script.js
+++ script.js
@@ -2,52 +2,98 @@ const regForm = document.querySelector('#regForm');
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
 const email = document.querySelector('#email');
+const output = document.querySelector('#outputList');
 
+const userList = [];
 
+const errorMessage = (input, message) => {
+    const parent = input.parentElement;
+    input.classList.remove('is-valid');
+    input.classList.add('is-invalid');
+    parent.querySelector('.invalid-feedback').innerText = message;
+}
 
-
+const success = input => {
+    input.classList.remove('is-invalid');
+    input.classList.add('is-valid');
+}
 
 const validateName = (name) => {
-    
-    
-
+    let regEx = /\d/;  // /\d/.test(string) search for digits from 0 to 9 in a string
+      
     if(name.value.trim() === '') {
-        name.classList.remove('is-valid');
-        name.classList.add('is-invalid');
-        name.parentElement.querySelector('.invalid-feedback').innerText = 'Du måste ange ett förnamn';
+        errorMessage(name,'Du måste ange ett för- och efternamn');
+        return false;
     } 
-    else if(name.value.trim().length < 2) {
-        name.classList.remove('is-valid');
-        name.classList.add('is-invalid');
-        name.parentElement.querySelector('.invalid-feedback').innerText = 'Ditt namn måste innehålla minst 2 tecken';
+    else if(name.value.trim().length < 2 || regEx.test(name.value)) {
+        errorMessage(name,'Ditt namn kan inte innehålla en siffra och måste var minst 2 tecken');
+        return false;
     } 
     else {
-        name.classList.remove('is-invalid');
-        name.classList.add('is-valid');
-       
+        success(name);
+        return true;
     }
 
+}
+
+const validateEmail = (email) => {
+    let regEx = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+    if(email.value.trim() === '') {
+        errorMessage(email,'Du måste ange en e-post adress');
+        return false;
+    }
+    else if(!regEx.test(email.value)) {
+        errorMessage(email,'Din e-post är felaktig försök igen');
+        return false;
+    }
+    else {
+        success(email);
+        return true;
+    }
+
+}
+
+const printUserList = (list) => {
+    output.innerHTML = '';
+    list.forEach(element => {
+        output.innerHTML += `
+            <li class="px-5 list-group-item">
+                <p class="m-0">${element.firstName + ' '+ element.lastName}</p>
+                <small><a href="#" class="card-link">${element.email}</a></small>
+            </li>
+        `;
+    });
 }
 
 
 regForm.addEventListener('submit', e => {
     e.preventDefault();
 
-
     //validera formuläret 
     validateName(firstName);
-    // validateName(lastName);
-    // validateEmail(email);
+    validateName(lastName);
+    validateEmail(email);
 
+    //skapar användare "user"
+    if(validateName(firstName) && validateName(lastName) && validateEmail(email)) {
+        const user = {
+            id: Date.now().toString(),
+            firstName: firstName.value,
+            lastName: lastName.value,
+            email: email.value,            
+        }
+        //lägger till användaren i listan "userList"
+        userList.unshift(user);
+        
+        //skriver ut listan 
+        printUserList(userList);
+    }
+    else {
+        
+    }
 
-
-    //skapa användare "user"
-
-    //lägga till i listan "userList"
     
-    // console.log(firstName.value);
-    // console.log(lastName.value);
-    // console.log(email.value);
 
 })
 
